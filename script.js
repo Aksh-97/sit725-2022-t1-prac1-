9ba74fc1cee7ecf03259ffb4e4a3d28ebd888955
Aksh-97 <101445571+Aksh-97@users.noreply.github.com>
2022-03-20 17:04:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function addTwoNumbers(input1, input2){
+    var x=document.getElementById(input1).value;
+    var y=document.getElementById(input2).value;
+    var sum=0;
+    sum=Number(x)+Number(y);
+    document.getElementById("printSum").style.fontWeight = 'bold';
+    document.getElementById("printSum").innerHTML = "SUM is : " + sum;
+}
+
